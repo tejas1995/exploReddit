84d02f08d1c140f7dfabb2469677175908d81935
Tejas Srinivasan <tejas.srinivasan95@gmail.com>
2016-06-30 03:16:58 -0400

    Added module to load data from userData.json into User objects

---
+++ src/loadUserData.py
@@ -0,0 +1,20 @@
+import json
+from user import User
+
+def loadUserData():
+    jsonData = open('../data/userData.json', 'r').read()
+
+    userData = json.loads(jsonData)
+
+    listUsers = []
+
+    for data in userData:
+        
+        u = User(data['username'], len(listUsers))
+        u.setNormScore(data['data'])
+        # print "User", u.username
+        # u.printScore()
+        # raw_input("Press Enter to continue...")
+        listUsers.append(u)
+
+    return listUsers
