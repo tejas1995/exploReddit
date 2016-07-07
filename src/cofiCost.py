c9da277a854c6094c80e4e5a61af85aaf5ab9f60
Tejas Srinivasan <tejas.srinivasan95@gmail.com>
2016-07-07 00:11:05 -0400

    Added collaborative filtering cost gradient method

--- src/cofiCost.py
+++ src/cofiCost.py
@@ -18,4 +18,19 @@ def cofiCostGrad(params, Y, R, numUsers, numSubs, numFeatures, lamda):
     X = params[:numSubs*numFeatures].reshape(numSubs, numFeatures)
     Theta = params[numSubs*numFeatures:].reshape(numUsers, numFeatures)
 
+    X_grad = np.zeros((numSubs, numFeatures))
+    Theta_grad = np.zeros((numUsers, numFeatures))
+
+    for i in range(numSubs):
+        X_grad[i] = np.dot((np.dot(X[i], Theta.T) - Y), Theta)
+        X_grad[i] += lamda*X[i]
+
+    for i in range(numUsers):
+        Theta_grad[i] = np.dot(X.T, (np.dot(X, Theta.T[:,i] - Y[:,i]))*R[:,i]).T
+        Theta_grad[i] += lamda*Theta[:,i]
+
+    res = np.concatenate((np.array(X_grad).ravel(), np.array(Theta_grad)))
+
+    return res 
+
     
